50a709c22416e101077ed358ae725c45442be361
jay270804 <jaypatel270804@gmail.com>
2025-05-20 04:21:30 +0530

    fix: cors blockage for client

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://aarogyaui.jaypatel.software',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Specify allowed headers
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
